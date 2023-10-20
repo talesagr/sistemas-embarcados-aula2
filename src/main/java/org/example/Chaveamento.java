4ee5a42ec314357a4a15f1d544854128520553bc
Tales Aguiar <talesdd@outlook.com>
2023-10-20 20:37:57 -0300

    aula 2 - Simulador de NoC em anel com N nodos

---
+++ src/main/java/org/example/Chaveamento.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Chaveamento {
+    private int id;
+
+    public Processamento findTarget(List<Processamento> processList,int target){
+        return processList.get(target);
+    }
+}
